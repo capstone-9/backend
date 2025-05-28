5793d2696c3ef0397cd36ee85124ee8b99b4efbe
Gyemoo <lgm528@naver.com>
2025-05-28 22:15:49 +0900

    stable diffusion 모델 수정

--- domain/AImodels/stablediffusion.py
+++ domain/AImodels/stablediffusion.py
@@ -9,10 +9,12 @@ from diffusers import AutoencoderKL, UNet2DConditionModel, DDPMScheduler
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-MODEL = "stabilityai/stable-diffusion-2-1"
-WEIGHT_PATH = BASE_DIR + "/StableDiffusion_42_ghibli_makoto_512_epoch100.pth"
+IMG_SIZE = 512
 
-# 모델 로딩
+MODEL = "stabilityai/stable-diffusion-2-1"
+WEIGHT_PATH = BASE_DIR + f"/StableDiffusion_42_ghibli_makoto_{IMG_SIZE}_epoch100_no_am.pth"
+
+
 tokenizer = CLIPTokenizer.from_pretrained(MODEL, subfolder="tokenizer")
 text_encoder = CLIPTextModel.from_pretrained(MODEL, subfolder="text_encoder").to(device)
 vae = AutoencoderKL.from_pretrained(MODEL, subfolder='vae').to(device)
@@ -21,7 +23,7 @@ unet.load_state_dict(torch.load(WEIGHT_PATH, map_location=device))
 noise_scheduler = DDPMScheduler.from_pretrained(MODEL, subfolder="scheduler")
 
 def generate_image(prompts: list[str], output_path: str = "output.png") -> str:
-    height, width = 512, 512
+    height, width = IMG_SIZE, IMG_SIZE
     num_inference_steps = 25
     guidance_scale = 7.5
     seed = 42
@@ -37,16 +39,14 @@ def generate_image(prompts: list[str], output_path: str = "output.png") -> str:
     )
 
     input_ids = text_input['input_ids'].to(device)
-    attention_mask = text_input['attention_mask'].to(device)
 
     with torch.no_grad():
-        text_embeddings = text_encoder(input_ids, attention_mask)[0]
+        text_embeddings = text_encoder(input_ids)[0]
 
     max_length = input_ids.shape[-1]
     uncond_input = tokenizer([""] * batch_size, padding="max_length", max_length=max_length, return_tensors="pt")
     uncond_embeddings = text_encoder(
         uncond_input['input_ids'].to(device),
-        uncond_input['attention_mask'].to(device)
     )[0]
 
     text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
